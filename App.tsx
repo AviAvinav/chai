5c7bbf1175ebfb879e6a34d429b2876dd48c4ecb
Avi Avinav <aviavinav2004@gmail.com>
2023-03-29 10:32:10 +0530

    add prettier config

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
