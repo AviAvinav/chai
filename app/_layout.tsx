42eff03aca52c44b846e65cc098a77796a3d569a
Avi Avinav <aviavinav2004@gmail.com>
2023-03-29 11:48:12 +0530

    add splash screen and update react-native(deps)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,41 @@
 import { Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
 
 const AppLayout = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
   return (
     <>
-      <Slot />
+      {!appIsReady ? (
+        <SafeAreaView className="flex-1 flex items-center justify-center">
+          <Text>Loading...</Text>
+        </SafeAreaView>
+      ) : (
+        <Slot />
+      )}
     </>
   );
 };
